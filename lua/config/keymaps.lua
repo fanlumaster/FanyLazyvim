9d573b7ea11ab1a5a61c56d6eaa55ea4ae9d4c65
fanlumaster <1730976608@qq.com>
2023-11-26 18:10:44 +0800

    add conform config for python formatter

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -70,6 +70,6 @@ map("n", "<leader>r", function()
 end, { desc = "Close current buffer" })
 
 -- toggleterm
-map("n", "<leader>th", "<cmd>ToggleTerm size=10 direction=horizontal<cr>", { desc = "ToggleTerm horizontal split" })
+map("n", "<leader>th", "<cmd>ToggleTerm size=15 direction=horizontal<cr>", { desc = "ToggleTerm horizontal split" })
 map("n", "<leader>tf", "<cmd>ToggleTerm direction=float<cr>", { desc = "ToggleTerm float" })
 map("n", "<leader>tv", "<cmd>ToggleTerm size=80 direction=vertical<cr>", { desc = "ToggleTerm vertical split" })
---
+++ lua/plugins/conform.lua
@@ -0,0 +1,10 @@
+return {
+  "stevearc/conform.nvim",
+  optional = true,
+  opts = {
+    formatters_by_ft = {
+      -- autopep8 似乎要比 black 要快一些
+      ["python"] = { "autopep8" },
+    },
+  },
+}
