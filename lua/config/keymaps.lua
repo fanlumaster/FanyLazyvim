d0867866661f5dfa9b4ddcf1a27c4edb91b0e78b
fanlumaster <1730976608@qq.com>
2023-11-30 23:23:46 +0800

    add cancel highlight keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -110,3 +110,6 @@ end, { desc = "Move current buffer to right" })
 
 -- for persisted
 map("n", "<leader>fs", "<cmd>Telescope persisted<cr>", { desc = "Search history sessions" })
+
+-- for cancel of highlight
+map("n", "<leader>h", "<cmd>noh<cr>", { desc = "Cancel highlight" })
