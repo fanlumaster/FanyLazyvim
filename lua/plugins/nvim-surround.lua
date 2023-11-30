612161cbc82b79e9e49d7af0d96b2eb5c998a0c2
fanlumaster <1730976608@qq.com>
2023-11-30 23:37:35 +0800

    fix warning caused by empty config of nvim-surround

--- lua/plugins/nvim-surround.lua
+++ lua/plugins/nvim-surround.lua
@@ -3,6 +3,19 @@ return {
   version = "*", -- Use for stability; omit to use `main` branch for the latest features
   event = "VeryLazy",
   config = function()
-    require("nvim-surround").setup({})
+    require("nvim-surround").setup({
+      keymaps = {},
+      surrounds = {},
+      aliases = {},
+      highlight = {},
+      move_cursor = "begin",
+      indent_lines = function(start, stop)
+        local b = vim.bo
+        -- Only re-indent the selection if a formatter is set up already
+        if start < stop and (b.equalprg ~= "" or b.indentexpr ~= "" or b.cindent or b.smartindent or b.lisp) then
+          vim.cmd(string.format("silent normal! %dG=%dG", start, stop))
+        end
+      end,
+    })
   end,
 }
