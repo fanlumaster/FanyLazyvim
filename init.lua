eecf44156a81928b439d13588768459d9f6d2520
fanlumaster <1730976608@qq.com>
2024-01-06 18:59:01 +0800

    fix copy relative path

--- init.lua
+++ init.lua
@@ -3,9 +3,9 @@ require("config.lazy")
 
 -- copy relative path
 vim.cmd([[
-command! -nargs=0 CpRelativePath lua require("lua.fany.fanyutils").copy_relative_path()
+command! -nargs=0 CpRelativePath lua require("fany.fanyutils").copy_relative_path()
 ]])
 -- copy relative path
 vim.cmd([[
-command! -nargs=0 CpFileName lua require("lua.fany.fanyutils").copy_current_filename()
+command! -nargs=0 CpFileName lua require("fany.fanyutils").copy_current_filename()
 ]])
