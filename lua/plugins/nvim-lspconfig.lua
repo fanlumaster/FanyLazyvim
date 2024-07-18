18f60653f631949f3471c10c7827a89d6c8697c6
fanlumaster <1730976608@qq.com>
2024-07-18 22:31:15 +0800

    resolve leader c remapping

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -4,9 +4,14 @@ return {
     local keys = require("lazyvim.plugins.lsp.keymaps").get()
     -- disable a keymap
     keys[#keys + 1] = { "<leader>ca", false }
+    keys[#keys + 1] = { "<leader>cc", false }
+    keys[#keys + 1] = { "<leader>cC", false }
+    keys[#keys + 1] = { "<leader>cR", false }
+    keys[#keys + 1] = { "<leader>cr", false }
     keys[#keys + 1] = { "<leader>cA", false }
     keys[#keys + 1] = { "<leader>cl", false }
-    keys[#keys + 1] = { "<leader>cr", false }
+    keys[#keys + 1] = { "<leader>cs", false }
+    keys[#keys + 1] = { "<leader>cS", false }
   end,
   opts = {
     servers = {
---
+++ lua/plugins/trouble.lua
@@ -0,0 +1,9 @@
+return {
+  "folke/trouble.nvim",
+  opts = {}, -- for default options, refer to the configuration section for custom setup.
+  cmd = "Trouble",
+  keys = {
+    { "<leader>cs", false },
+    { "<leader>cS", false },
+  },
+}
