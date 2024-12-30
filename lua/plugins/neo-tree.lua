7d164d34e43d109e27cfcbda14a9348929dbeab2
fanlumaster <1730976608@qq.com>
2024-12-31 01:18:25 +0800

    update neotree keymaps

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,36 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  cmd = "Neotree",
+  keys = {
+    {
+      "<leader>fe",
+      function()
+        require("neo-tree.command").execute({ toggle = true, reveal = true, dir = LazyVim.root() })
+      end,
+      desc = "Explorer NeoTree (Root Dir)",
+    },
+    {
+      "<leader>fE",
+      function()
+        require("neo-tree.command").execute({ toggle = true, reveal = true, dir = vim.uv.cwd() })
+      end,
+      desc = "Explorer NeoTree (cwd)",
+    },
+    { "<leader>e", "<leader>fe", desc = "Explorer NeoTree (Root Dir)", remap = true },
+    { "<leader>E", "<leader>fE", desc = "Explorer NeoTree (cwd)", remap = true },
+    {
+      "<leader>ge",
+      function()
+        require("neo-tree.command").execute({ source = "git_status", toggle = true })
+      end,
+      desc = "Git Explorer",
+    },
+    {
+      "<leader>be",
+      function()
+        require("neo-tree.command").execute({ source = "buffers", toggle = true })
+      end,
+      desc = "Buffer Explorer",
+    },
+  },
+}
